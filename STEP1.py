38848fa2761c284174e4f362c6c7b691d8f917c9
masaru <furukawamasaru08@gmail.com>
2026-04-05 07:30:00 +0900

    No2

--- STEP1.py
+++ STEP1.py
@@ -1,2 +1,14 @@
 data = [10, 20, 30, 40]
 print(sum(data))
+
+# dataの個数を出す 
+print(len(data))
+
+# dataの平均を出す
+average = sum(data) / len(data) 
+print(average)
+
+# for文でdataの値を1つずつ出力する
+for value in data:
+    print(value)    
+    
