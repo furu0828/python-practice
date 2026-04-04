6b8e53ad74f41acfff8e84a8ee34632be4fd059f
masaru <furukawamasaru08@gmail.com>
2026-04-05 08:07:55 +0900

    No2

--- STEP4.py
+++ STEP4.py
@@ -3,7 +3,17 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame({
     "age": [20, 25, 30, 35, 40, 45, 50]
+    "income": [300, 350, 500, 650, 800]
 })
 
 plt.hist(df["age"])
 plt.show()
+
+
+# ageのbinsを5にしてヒストグラムを描く
+plt.hist(df["age"], bins=5) 
+plt.show()  
+
+# ageとincomeの散布図を描く
+plt.scatter(df["age"], df["income"])        
+plt.show()  
