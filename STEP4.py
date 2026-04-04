4d4ccba6068ca0c0638e257f83a1a70323ed68e4
masaru <furukawamasaru08@gmail.com>
2026-04-05 08:09:14 +0900

    No3

--- STEP4.py
+++ STEP4.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.DataFrame({
-    "age": [20, 25, 30, 35, 40, 45, 50]
+    "age": [20, 25, 30, 35, 40, 45, 50],
     "income": [300, 350, 500, 650, 800]
 })
 
