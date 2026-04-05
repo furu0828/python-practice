27cef79586d49b72c0c2a010045e23e3dab87aa7
masaru <furukawamasaru08@gmail.com>
2026-04-06 07:12:28 +0900

    No2

--- STEP5.py
+++ STEP5.py
@@ -1,8 +1,26 @@
+import matplotlib.pyplot as plt 
 import pandas as pd
 
 df = pd.DataFrame({
     "age": [20, 25, 30, 35, 40],
     "income": [300, 350, 500, 650, 800]
 })
+
+
+
+
+# study_time 列を追加して、他の列との相関を見る
+df["study_time"] = [1, 2, 2, 3, 5]
+
+# ageとincomeの相関をmatplotlibで可視化
+plt.scatter(df["age"], df["income"])
+plt.xlabel("Age")       
+plt.ylabel("Income")
+plt.title("Age vs Income")      
+plt.show()  
+
+
+
+
 
 print(df.corr())
